8764c4e173d715e99f7dad54b340ffa87efd5c6b
vyacheslav-sh <vs8629239@gmail.com>
2021-11-25 19:23:35 +0300

    Money Transfer

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	int accountSum = 2_000_000_000;
+    int transferSum = 500_000_000;
+    int total = accountSum + transferSum;
+    System.out.println(total);
+    }
+}
