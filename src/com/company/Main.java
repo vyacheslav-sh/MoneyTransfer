e2a43e783fca7bbd5bf8e62c226b67b921c71586
Alexey Boyko <BoykoAA@hq.tatneft.ru>
2022-01-04 12:21:26 +0300

    gitignore

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-	int accountSum = 2_000_000_000;
-    int transferSum = 500_000_000;
-    int total = accountSum + transferSum;
-    System.out.println(total);
+        int accountSum = 2_000_000_000;
+        int transferSum = 500_000_000;
+        int total = accountSum + transferSum;
+        System.out.println(total);
     }
 }
